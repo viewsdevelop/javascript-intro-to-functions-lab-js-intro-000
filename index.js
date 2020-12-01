e5f3f270a3cff0b98b16ab8456a688d71b61f4a3
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-01 06:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if string.toUpperCase === string {
     return "YES INDEED!"
-  }
+  } else if string === "I love you, Grandma."
 }
